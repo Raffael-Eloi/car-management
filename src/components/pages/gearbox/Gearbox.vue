5adcb0d7c3f65d4bbf894766861458cc2b72cdc7
Raffael Silva <raffaeleloi121@gmail.com>
2022-07-20 19:33:37 -0300

    adding pagination and filter to gearboxes

--- src/components/pages/gearbox/Gearbox.vue
+++ src/components/pages/gearbox/Gearbox.vue
@@ -8,18 +8,29 @@
 
       <b-col md="9">
         <b-row class="mt-3" v-if="!isModalOpen()">
-          <b-col md="8">
+          <b-col md="2">
+            <b-form-select v-model="table.filter.attributeSearch" :options="filterAttributeOptions"></b-form-select>
+          </b-col>
+          <b-col md="1">
+            <b-button variant="danger" @click="cleanFilter()">Limpar</b-button>
+          </b-col>
+          <b-col md="5">
             <b-row>
               <b-col md="10">
                 <b-form-input
                   type="text"
-                  placeholder="Pesquisar caixa de marchas"
+                  placeholder="Pesquisar"
+                  v-model="table.filter.keywords"
                 ></b-form-input>
               </b-col>
               <b-col md="2">
-                <b-button variant="success"
-                  ><i class="fa-solid fa-magnifying-glass"></i
-                ></b-button>
+                <b-button
+                  variant="success"
+                  @click="searchGearbox()"
+                  :disabled="(table.filter.keywords === '' || table.filter.attributeSearch === '')"
+                >
+                  <i class="fa-solid fa-magnifying-glass"></i>
+                </b-button>
               </b-col>
               <br />
             </b-row>
@@ -39,12 +50,26 @@
 
         <b-row class="px-5 mt-5" v-if="!isModalOpen()">
           <b-col md="4">
+            <b-row>
+              <b-col md="4">
+                <b-form-select v-model="table.filter.perPage">
+                  <b-form-select-option :value="10">10</b-form-select-option>
+                  <b-form-select-option :value="25">25</b-form-select-option>
+                  <b-form-select-option :value="50">50</b-form-select-option>
+                  <b-form-select-option :value="100">100</b-form-select-option>
+                </b-form-select>
+              </b-col>
+              <b-col md="8" class="mt-2">
+                Resultados por página
+              </b-col>
+            </b-row>
+          </b-col>
+          <b-col md="4"></b-col>
+          <b-col md="4">
             <b-button variant="secondary" @click="getGerBoxes()">
               Atualizar tabela <i class="fa-solid fa-arrows-rotate"></i>
             </b-button>
           </b-col>
-          <b-col md="4"></b-col>
-          <b-col md="4"></b-col>
         </b-row>
 
         <h5 class="text-center mt-5" v-if="!isModalOpen()">
@@ -92,17 +117,28 @@
                   </b-td>
                 </b-tr>
               </b-tbody>
-              <b-tfoot>
-                <b-tr>
-                  <b-td colspan="5" variant="success" class="text-end"
-                    ><span class="me-5"
-                      >Total de caixa de marchas:
-                      <b>{{ table.data.length }}</b></span
-                    ></b-td
-                  >
-                </b-tr>
-              </b-tfoot>
             </b-table-simple>
+          </b-col>
+        </b-row>
+
+        <b-row v-if="!loading && !isModalOpen()">
+          <b-col md="8"></b-col>
+          <b-col md="4">
+            <b-button-group>
+              <b-button
+                @click="previousPage()"
+                :disabled="this.table.filter.page === 1"
+              >
+                Anterior
+              </b-button>
+              <b-button>{{ table.filter.page }}</b-button>
+              <b-button 
+                @click="nextPage()"
+                :disabled="table.filter.nextPage === null"
+              >
+                Próximo
+              </b-button>
+            </b-button-group>
           </b-col>
         </b-row>
 
@@ -162,8 +198,21 @@ export default {
   data() {
     return {
       table: {
+        filter: {
+          perPage: 10,
+          page: 1,
+          nextPage: null,
+          keywords: '',
+          attributeSearch: '',
+          filterByAttribute: false
+        },
         data: [],
       },
+
+      filterAttributeOptions: [
+        {value: null, text: 'Filtro'},
+        {value: 'name', text: 'Nome'}
+      ],
 
       modal: {
         openCreateGearboxModal: false,
@@ -249,10 +298,11 @@ export default {
           common: {
             Authorization: `Bearer ${this.token}`,
           }
-        }
+        },
+        params: this.table.filter
       })
         .then((response) => {
-          this.table.data = response.data;
+          this.table.data = response.data.data;
           this.loading = false;
         })
         .catch((error) => console.log(error));
@@ -300,6 +350,32 @@ export default {
         "success"
       );
     },
+
+    previousPage() {
+      if (this.table.filter.page !== 1) {
+        this.table.filter.page -= 1;
+        this.getGerBoxes();
+      }
+    },
+
+    nextPage() {
+      if (this.table.filter.nextPage !== null) {
+        this.table.filter.page += 1;
+        this.getGerBoxes();
+      }
+    },
+
+    searchGearbox () {
+      this.table.filter.filterByAttribute = true;
+      this.getGerBoxes();
+    },
+
+    cleanFilter() {
+      this.table.filter.attributeSearch = '';
+      this.table.filter.filterByAttribute = false;
+      this.table.filter.keywords = '';
+      this.getGerBoxes();
+    }
   },
 };
 </script>
